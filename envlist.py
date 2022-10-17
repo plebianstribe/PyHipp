4d50582f2b9471a5e63f4ebe835924d092292188
AWS ParallelCluster user <ec2-user@ip-10-0-0-181.ap-southeast-1.compute.internal>
2022-10-17 09:38:54 +0000

    added envlist.py to generate multiple conda envs

--- envlist.py
+++ envlist.py
@@ -42,17 +42,21 @@ with lock:
     if pmode == RESET_MODE:
         # create a list (named clist) of nevns environments with the 
         # prefix envprefix
-        # add code here
+#mycode
+        clist = [str(envprefix)+str(x) for x in range(int(nenvs))]
     else:
         # load hickle file
         clist = hickle.load(file_path)
 
         if pmode == WRITE_MODE:
             # append item to end of list
-            # add code here
+#mycode
+            clist.append(env)
         else:    
             # get and remove env from clist
-            # add code here
+#mycode
+            env = clist[0] 
+            clist.remove(env)
             # return env name
             print(env)
 
